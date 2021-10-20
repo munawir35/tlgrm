e382033c629dbd72c2cc884e52b7f94fad1fed54
munawir4 <munawir4@gmail.com>
2021-10-20 09:53:22 +0530

    new

---
+++ resources/views/view.blade.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <style>
+        .new {
+            background-color: rgb(96, 194, 96);
+            height: 100px;
+        }
+        
+        .img {
+            font-size: 40px;
+            color: white;
+        }
+        
+        .cl {
+            color: white;
+        }
+        
+        .w {
+            margin-left: 1200px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar new ">
+        <div class="container-fluid">
+            <a class="navbar-brand img" href="#">
+                <img src="1.jpg" alt="" width="80" height="60" class="d-inline-block align-text-top"> Telegram
+            </a>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+    <div class="container"><br><br>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Canal liverpool turff
+                        </h5>
+                        <p class="card-text"><select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                        <option selected>Available Slots</option>
+                        <option value="1">7:00 to 8:00 pm</option>
+                        <option value="2">9:00 to 10:00 pm</option>
+                        <option value="3">11:00 to 12:00 pm</option>
+                        <option value="3">1:00 to 7:00 am</option>
+                      </select></p>
+                        <a href="#" class="btn btn-primary">Book</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="6.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Palazhi turff</h5>
+                        <p class="card-text"><select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                        <option selected>Available Slots</option>
+                        <option value="1">8:00 to 9:00 pm</option>
+                        <option value="2">9:00 to 11:00 pm</option>
+                        <option value="3">1:00 to 4:00 am</option>
+                      </select></p>
+                        <a href="#" class="btn btn-primary">Book</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">marakkana turff</h5>
+                        <p class="card-text"><select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                        <option selected>Available Slots</option>
+                        <option value="1">8:00 to 9:00 pm</option>
+                        <option value="2">10:00 to 11:00 pm</option>
+                        <option value="3">1:00 to 4:00 am</option>
+                        <option value="3">7:00 to 8:00 am</option>
+                      </select></p>
+                        <a href="#" class="btn btn-primary">Book</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div><br>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="11.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">anfield turff</h5>
+                        <p class="card-text"><select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                        <option selected>Available Slots</option>
+                        <option value="1">7:00 to 9:00 pm</option>
+                        <option value="2">10:00 to 11:00 pm</option>
+                        <option value="3">1:00 to 4:00 am</option>
+                        <option value="3">7:00 to 8:00 am</option>
+                      </select></p>
+                        <a href="#" class="btn btn-primary">Book</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="8.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Malabar Sports Park</h5>
+                        <p class="card-text"><select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                        <option selected>Available Slots</option>
+                        <option value="1">7:00 to 9:00 pm</option>
+                        <option value="2">10:00 to 11:00 pm</option>
+                        <option value="3">1:00 to 4:00 am</option>
+                        <option value="3">7:00 to 8:00 am</option>
+                      </select></p>
+                        <a href="#" class="btn btn-primary">Book</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="10.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Camp nou</h5>
+                        <p class="card-text"><select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                        <option selected>Available Slots</option>
+                        <option value="1">7:00 to 9:00 pm</option>
+                        <option value="2">10:00 to 11:00 pm</option>
+                        <option value="3">1:00 to 4:00 am</option>
+                        <option value="3">7:00 to 8:00 am</option>
+                      </select></p>
+                        <a href="#" class="btn btn-primary">Book</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div><br><br>
+    <button type="button" class="btn btn-success w"><a href="http://127.0.0.1:8000/new" class="cl">Back</a></button>
+</body>
+
+</html>
